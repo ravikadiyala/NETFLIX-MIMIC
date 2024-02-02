7a4b62eb29ee081b683037bda25a2aa9b662f600
RAVINDRA KADIYALA <ravikadiyala@RAVINDRAs-MacBook-Air.local>
2024-02-02 16:06:59 +0530

    Changed title of App

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -2,13 +2,13 @@ import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
 
 const GptMovieSuggestions = () => {
-    const gpt = useSelector(store => store.gpt)
+    const gpt = useSelector(store => store?.gpt)
     const {movieNames, movieResults} = gpt
     return (
         <div className="p-4 m-4 bg-black bg-opacity-80">
             <div>
                 {
-                    movieNames.map((movie, index) => {
+                    movieNames && movieNames.map((movie, index) => {
                         return <MovieList key = {movie} title = {movie} movies = {movieResults[index]}/>
                     })
                 }
